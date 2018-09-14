356938bfa53426012add61ae9fa7bdfad0ef3026
Ryan Wood <Ryan.Wood01@bbc.co.uk>
2018-09-14 17:12:43 +0100

    Add description to the project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject crimpjure "0.0.1-SNAPSHOT"
-  :description "Cool new project to do things and stuff"
+  :description "A clojure implementation of Crimp"
   :dependencies [[org.clojure/clojure "1.9.0"] [cheshire "5.8.0"] [pandect "0.6.1"]]
   :profiles {:dev {:dependencies [[midje "1.9.2"]]}
              ;; You can add dependencies that apply to `lein midje` below.
